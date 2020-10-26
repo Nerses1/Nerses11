76aa7280dbab02801edde391c36370d373d0c003
kbstudent internet access <kbstudentint@edu.fa.ru>
2020-10-26 10:39:29 +0300

    ***

---
+++ Задача 4.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+#include <iostream.h>
+#include <math.h>
+int main(){
+	int x, y;
+	double a = 345.345;
+	double b, c;
+	bool m;
+	//cin >> a;
+	c = modf(a, &b);
+	while (c<b) {
+		c *= 10;
+	}
+	m = (b - c>0);
+	cout << b << endl << c << endl << m;
+	getchar();
+	return 0;
+}
