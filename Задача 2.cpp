0658624e98152e2fa916e98741301a56d71a75e5
kbstudent internet access <kbstudentint@edu.fa.ru>
2020-10-26 10:31:46 +0300

    ****

---
+++ Задача 2.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include <iomanip>
+#include <iostream>
+
+constexpr bool Check(const int x)
+{
+	return x / 1000 == x % 10 && x / 100 % 10 == x / 10 % 10;
+}
+
+int main()
+{
+	std::cout << std::boolalpha << Check(1221) << std::endl;
+}
